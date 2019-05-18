4f6c1cbc7fe7fcf04cbd091abd4ba8038d898a2a
Daniel Shotonwa <danielshotonwa53@gmail.com>
2019-05-18 23:11:52 +0100

    add me query to get logged in user

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -6,8 +6,17 @@ module Types
     # TODO: remove me
     field :all_events, [EventType], null: false,
       description: "Get all events from the database"
+    field :me, [UserType], null: true,
+      description: "Get information on the logged in user"
     def all_events
       Event.all
+    end
+
+    def me
+      if context[:current_user].nil?
+        return GraphQL::ExecutionError.new("You are not logged in")
+      end
+      context[:current_user]
     end
   end
 end
