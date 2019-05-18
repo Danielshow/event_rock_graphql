876c78c601ef36bad4010b27d8840ae6a3693a9d
Daniel Shotonwa <danielshotonwa53@gmail.com>
2019-05-18 23:48:23 +0100

    add some validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true,
+                    uniqueness: true,
+                    format: {
+                      with: /[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}/
+                    }
 
   has_many :events
+  before_save :email_downcase
+
+  def email_downcase
+    self.email = email.downcase
+  end
 end
