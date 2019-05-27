d6cf268d03084d1ec3e661f088c4d6288a238d62
Daniel Shotonwa <danielshotonwa53@gmail.com>
2019-05-27 15:23:19 +0100

    implement pagination on event

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,21 +1,25 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
 
-    # TODO: remove me
     field :all_events, [EventType], null: false,
-      description: "Get all events from the database"
+                                    description: 'Get all events from the database'
     field :me, [UserType], null: true,
-      description: "Get information on the logged in user"
+                           description: 'Get information on the logged in user'
+    field :event_connection, EventType.connection_type, null: false
+
     def all_events
+      Event.all
+    end
+
+    def event_connection
       Event.all
     end
 
     def me
       if context[:current_user].nil?
-        return GraphQL::ExecutionError.new("You are not logged in")
+        return GraphQL::ExecutionError.new('You are not logged in')
       end
+
       context[:current_user]
     end
   end
